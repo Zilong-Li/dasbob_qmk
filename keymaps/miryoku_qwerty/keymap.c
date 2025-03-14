149bfe42ff1f4c5a1e22f563485d03e69f0d015a
Zilong-Li <zimusen94@gmail.com>
2025-03-14 21:36:05 +0100

    vim nav

--- keymaps/miryoku_qwerty/keymap.c
+++ keymaps/miryoku_qwerty/keymap.c
@@ -54,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NAVIGATION] = LAYOUT_split_3x5_3(
             KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_AGIN,      KC_PSTE,      KC_COPY,        KC_CUT,       KC_UNDO,
-            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_CAPS,      KC_LEFT,      KC_DOWN,        KC_UP,        KC_RIGHT,
+            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_LEFT,      KC_DOWN,      KC_UP,          KC_RIGHT,     KC_CAPS, 
             KC_RALT,        KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_INSERT,    KC_HOME,      KC_PGDN,        KC_PGUP,      KC_END,
                                           KC_NO,        KC_NO,        KC_NO,             KC_ENTER,     KC_BSPC,      KC_DELETE
     ),
 
     [MOUSE] = LAYOUT_split_3x5_3(
             KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_AGIN,      KC_PSTE,      KC_COPY,        KC_CUT,       KC_UNDO,
-            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_NO,        KC_MS_L,      KC_MS_D,        KC_MS_U,      KC_MS_R,
+            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_MS_L,      KC_MS_D,      KC_MS_U,        KC_MS_R,      KC_NO, 
             KC_RALT,        KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_NO,        KC_WH_L,      KC_WH_D,        KC_WH_U,      KC_WH_R,
                                           KC_NO,        KC_NO,        KC_NO,             KC_BTN2,      KC_BTN1,      KC_BTN3
     ),
