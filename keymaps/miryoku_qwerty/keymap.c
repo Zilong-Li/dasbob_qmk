8eaf88cd247a7734ae35058d5d60f6dcb67511ea
Zilong-Li <zimusen94@gmail.com>
2025-03-16 23:37:10 +0100

    no gui on pinky

--- keymaps/miryoku_qwerty/keymap.c
+++ keymaps/miryoku_qwerty/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [BASE] = LAYOUT_split_3x5_3(
             KC_Q,           KC_W,         KC_E,         KC_R,         KC_T,              KC_Y,         KC_U,         KC_I,           KC_O,         KC_P,
-            LGUI_T(KC_A),   LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,              KC_H,         LSFT_T(KC_J), LCTL_T(KC_K),   LALT_T(KC_L), LGUI_T(KC_SCLN),
+            KC_A,   LALT_T(KC_S), LCTL_T(KC_D), LGUI_T(KC_F), LSFT_T(KC_G),              LSFT_T(KC_H), LGUI_T(KC_J), LCTL_T(KC_K),   LALT_T(KC_L), KC_SCLN,
             RALT_T(KC_Z),   KC_X,         KC_C,         KC_V,         KC_B,              KC_N,         KC_M,         KC_COMMA,       KC_DOT,       RALT_T(KC_SLASH),
                                           ESC_MED,      SPC_NAV,      TAB_CUR,           ENT_SYM,      BSP_NUM,      DEL_FUN
     ),
@@ -47,21 +47,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // the following three layers are related to the left thumb
     [MEDIA] = LAYOUT_split_3x5_3(
             KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,             RGB_TOG,      RGB_MOD,      RGB_HUI,        RGB_SAI,      RGB_SPI,
-            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             RGB_VAI,      KC_MPRV,      KC_VOLD,        KC_VOLU,      KC_MNXT,
+            KC_NO,          KC_LALT,      KC_LCTL,      KC_LGUI,      KC_LSFT,           RGB_VAI,      KC_MPRV,      KC_VOLD,        KC_VOLU,      KC_MNXT,
             KC_RALT,        KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_NO,        KC_NO,        KC_BRID,        KC_BRIU,      KC_NO,
                                           KC_NO,        KC_NO,        KC_NO,             KC_MSTP,      KC_MPLY,      KC_MUTE
     ),
 
     [NAVIGATION] = LAYOUT_split_3x5_3(
             KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_AGIN,      KC_PSTE,      KC_COPY,        KC_CUT,       KC_UNDO,
-            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_LEFT,      KC_DOWN,      KC_UP,          KC_RIGHT,     KC_CAPS, 
+            KC_NO,          KC_LALT,      KC_LCTL,      KC_LGUI,      KC_LSFT,           KC_LEFT,      KC_DOWN,      KC_UP,          KC_RIGHT,     KC_CAPS, 
             KC_RALT,        KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_INSERT,    KC_HOME,      KC_PGDN,        KC_PGUP,      KC_END,
                                           KC_NO,        KC_NO,        KC_NO,             KC_ENTER,     KC_BSPC,      KC_DELETE
     ),
 
     [MOUSE] = LAYOUT_split_3x5_3(
             KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_AGIN,      KC_PSTE,      KC_COPY,        KC_CUT,       KC_UNDO,
-            KC_LGUI,        KC_LALT,      KC_LCTL,      KC_LSFT,      KC_NO,             KC_MS_L,      KC_MS_D,      KC_MS_U,        KC_MS_R,      KC_NO, 
+            KC_NO,          KC_LALT,      KC_LCTL,      KC_LGUI,      KC_LSFT,           KC_MS_L,      KC_MS_D,      KC_MS_U,        KC_MS_R,      KC_NO, 
             KC_RALT,        KC_NO,        KC_NO,        KC_NO,        KC_NO,             KC_NO,        KC_WH_L,      KC_WH_D,        KC_WH_U,      KC_WH_R,
                                           KC_NO,        KC_NO,        KC_NO,             KC_BTN2,      KC_BTN1,      KC_BTN3
     ),
@@ -69,21 +69,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // and the following three are from the right thumb
     [SYMBOLS] = LAYOUT_split_3x5_3(
             KC_LCBR,        LSFT(KC_7),   LSFT(KC_8),   LSFT(KC_9),   KC_RCBR,           KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,
-            LSFT(KC_QUOTE), LSFT(KC_4),   LSFT(KC_5),   LSFT(KC_6),   LSFT(KC_EQL),      KC_NO,        KC_LSFT,      KC_LCTL,        KC_LALT,      KC_LGUI,
+            LSFT(KC_QUOTE), LSFT(KC_4),   LSFT(KC_5),   LSFT(KC_6),   LSFT(KC_EQL),      KC_LSFT,      KC_LGUI,      KC_LCTL,        KC_LALT,      KC_NO,
             LSFT(KC_GRAVE), LSFT(KC_1),   LSFT(KC_2),   LSFT(KC_3),   LSFT(KC_BSLS),     KC_NO,        KC_NO,        LSFT(KC_COMMA), LSFT(KC_DOT), KC_RALT,
                                           LSFT(KC_9),   LSFT(KC_0),   LSFT(KC_MINUS),    KC_NO,        KC_NO,        KC_NO
     ),
 
     [NUMBERS] = LAYOUT_split_3x5_3(
             KC_LBRC,        KC_7,         KC_8,         KC_9,         KC_RBRC,           KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,
-            KC_QUOTE,       KC_4,         KC_5,         KC_6,         KC_EQUAL,          KC_NO,        KC_LSFT,      KC_LCTL,        KC_LALT,      KC_LGUI,
+            KC_QUOTE,       KC_4,         KC_5,         KC_6,         KC_EQUAL,          KC_LSFT,      KC_LGUI,      KC_LCTL,        KC_LALT,      KC_NO,
             KC_GRAVE,       KC_1,         KC_2,         KC_3,         KC_BSLS,           KC_NO,        KC_NO,        KC_COMMA,       KC_DOT,       KC_RALT,
                                           KC_DOT,       KC_0,         KC_MINUS,          KC_NO,        KC_NO,        KC_NO
     ),
 
     [FUNCTIONS] = LAYOUT_split_3x5_3(
             KC_F12,         KC_F7,        KC_F8,        KC_F9,        KC_PSCR,           KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,
-            KC_F11,         KC_F4,        KC_F5,        KC_F6,        KC_SCRL,           KC_NO,        KC_LSFT,      KC_LCTL,        KC_LALT,      KC_LGUI,
+            KC_F11,         KC_F4,        KC_F5,        KC_F6,        KC_SCRL,           KC_LSFT,      KC_LGUI,      KC_LCTL,        KC_LALT,      KC_NO,
             KC_F10,         KC_F1,        KC_F2,        KC_F3,        KC_PAUSE,          KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_RALT,
                                           KC_NO,        KC_APP,       KC_NO,             KC_NO,        KC_NO,        KC_NO
     ),
