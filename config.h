916c72557a9c418e33857df266941f03e6d78d57
Zilong-Li <zimusen94@gmail.com>
2026-03-09 08:41:07 +0100

    update mouse settings

--- config.h
+++ config.h
@@ -41,15 +41,15 @@
 #define MOUSEKEY_INTERVAL 16
 // The default is 20. Since we made the mouse about 3 times faster with the previous setting,
 // give it more time to accelerate to max speed to retain precise control over short distances.
-#define MOUSEKEY_TIME_TO_MAX 40
+#define MOUSEKEY_TIME_TO_MAX 30
 // The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-#define MOUSEKEY_DELAY 100
+#define MOUSEKEY_DELAY 10
 // It makes sense to use the same delay for the mouseweel
-#define MOUSEKEY_WHEEL_DELAY 100
-// The default is 100
+#define MOUSEKEY_WHEEL_DELAY 10
+// The default is 80
 #define MOUSEKEY_WHEEL_INTERVAL 50
 // The default is 40
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
 // Pick good defaults for enabling homerow modifiers
 #define TAPPING_TERM 200
